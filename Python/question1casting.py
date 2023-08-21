b58fbf45ed54fa38d17676ce46a15fb23342d6d7
kumar <kumardhayait15@gmail.com>
2023-08-21 10:19:27 +0530

    python

---
+++ Python/question1casting.py
@@ -0,0 +1,18 @@
+####example  Get input variable name and age and print it
+name=input()
+age=input()
+address=input()
+print("May Name is:", name)
+print("My age is:", age)
+print("Address is:", address)
+
+
+##Get integer input for varible a,b,c  multiply all 3 and add all 3 and divide the multiply value by added value
+
+a=int(input())
+b=int(input())
+c=int(input())
+d=a*b*c
+e=a+b+c
+f=d/e
+print(f)
---
+++ Python/questions2 case.py
@@ -0,0 +1,18 @@
+#########Get input variable name, score and department get score of 100 and divide by 10'
+name=input()
+score=int(input())
+department=input()
+print("my name is:", name)
+print("my score is:", score/10,"/10")
+print("My department is:", department)
+
+
+"""sample output
+kumar
+89
+csc
+my name is: kumar
+my score is: 8.9
+My department is: csc
+"""
+
