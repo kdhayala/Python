5fede61e74442c0cac70bb0b7bf609bb23aee2c1
kumar <kumardhayait15@gmail.com>
2023-08-22 16:57:03 +0530

    22-08-2023

---
+++ Python/forloopex.py
@@ -0,0 +1,18 @@
+# For loop example i=variable
+"""
+for i in "apple":
+    print(i)
+
+
+    
+# range function  it going to check 0 to before defined number.
+
+for i in range(6):
+    print(i)"""
+
+# 2 tables print...
+for i in range(1,21):
+    print(i, "x 2 =", i*3)
+    
+
+
---
+++ Python/ifelseexcercise.py
@@ -0,0 +1,40 @@
+"""mark=int(input())
+if(mark>35):
+    print("pass")
+else:
+    print("fail") 
+
+#Get input for varibale income and greater than 7000 not eligilble for colorship
+
+
+income=int(input("Please enter Income"))
+if(income>7000):
+    print("Not eligible for scholarship")
+else:
+    print("Eligibe for scholorship") 
+
+
+# to check divisable number to get modular
+a=16
+print(a%3)
+
+
+
+#Check the number divisable by 3 and 5, we are going to use binary operator
+
+and operator
+a=int(input())
+if(a%3==0 and a%5==0):
+   print(" Divible by 3 and 5")
+else:
+    print("Not Divisable by 3 and 5")"""
+
+
+# OR Operator
+a=int(input())
+if(a%3==0 or a%5==0):
+   print(" Divible by 3 and 5")
+else:
+    print("Not Divisable by 3 and 5")
+
+
